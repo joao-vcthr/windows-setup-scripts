b42cf7020976fa8be657de2ea85633045c7d24db
joao-vcthr <joaovicthorm1@gmail.com>
2026-06-07 19:37:18 -0300

    Change language to en-US

--- functions/Set/Set-KeyboardLayout.ps1
+++ functions/Set/Set-KeyboardLayout.ps1
@@ -1,6 +1,6 @@
 function Set-KeyboardLayout {
     Write-Host "==> Removing unused keyboard layouts..." -ForegroundColor Cyan
-    $LanguageList = New-WinUserLanguageList "pt-BR"
+    $LanguageList = New-WinUserLanguageList "en-US"
     $LanguageList[0].InputMethodTips.Clear()
     $LanguageList[0].InputMethodTips.Add("0416:00010416")
     Set-WinUserLanguageList -LanguageList $LanguageList -Force
