5d7d01ac07a55b1722581596f037a50736e00dab
joao-vcthr <joaovicthorm1@gmail.com>
2026-06-08 19:22:53 -0300

    Remove useless configurations, reorder Select-Powerplan options

--- functions/Set/Set-PowerPlan.ps1
+++ functions/Set/Set-PowerPlan.ps1
@@ -9,7 +9,6 @@ function Set-PowerPlanHighPerformance {
     powercfg /change standby-timeout-ac 0 # Disable Suspension
 
     powercfg /setacvalueindex SCHEME_CURRENT 02f815b5-a5cf-4c84-bf20-649d1f75d3d8 4c793e7d-a264-42e1-87d3-7a0d2f523ccd 000 # JavaScript Timer Frequency
-    powercfg /setacvalueindex SCHEME_CURRENT 19cbb8fa-5279-450e-9fac-8a3d5fedd0c1 12bbebe6-58d6-4636-95bb-3217ef867c1a 003 # Power Saving Mode
     powercfg /setacvalueindex SCHEME_CURRENT 2a737441-1930-4402-8d77-b2bebba308a3 48e6b7a6-50f5-4782-a5d4-53bb8f07e226 000 # USB selective suspend setting
     powercfg /setacvalueindex SCHEME_CURRENT SUB_PCIEXPRESS ASPM 000 # USB PCI Express ASPM
 
@@ -29,7 +28,6 @@ function Set-PowerPlanBalanced {
     powercfg /change standby-timeout-ac 0 # Disable Suspension
 
     powercfg /setacvalueindex SCHEME_CURRENT 02f815b5-a5cf-4c84-bf20-649d1f75d3d8 4c793e7d-a264-42e1-87d3-7a0d2f523ccd 000 # JavaScript Timer Frequency
-    powercfg /setacvalueindex SCHEME_CURRENT 19cbb8fa-5279-450e-9fac-8a3d5fedd0c1 12bbebe6-58d6-4636-95bb-3217ef867c1a 003 # Power Saving Mode
     powercfg /setacvalueindex SCHEME_CURRENT 2a737441-1930-4402-8d77-b2bebba308a3 48e6b7a6-50f5-4782-a5d4-53bb8f07e226 000 # USB selective suspend setting
     powercfg /setacvalueindex SCHEME_CURRENT SUB_PCIEXPRESS ASPM 000 # USB PCI Express ASPM
 
@@ -45,7 +43,7 @@ function Select-PowerPlan {
 
     switch ($power) {
         1 { Set-PowerPlanBalanced }
-        2 { Set-PowerPlanHighPerformance}
+        2 { Set-PowerPlanHighPerformance }
         Default { Write-Host "Invalid Option" -ForegroundColor Red ; Select-PowerPlan }
     }
 }
