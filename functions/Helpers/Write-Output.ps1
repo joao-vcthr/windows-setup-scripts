09ef4a62948ad72039699474e00bd3d290c7c37f
joao-vcthr <joaovicthorm1@gmail.com>
2026-06-16 02:44:02 -0300

    change to ascii characters to avoid encoding bugs

--- functions/Helpers/Write-Output.ps1
+++ functions/Helpers/Write-Output.ps1
@@ -1,10 +1,25 @@
 function Write-Header {
     param ([string] $Title)
     Write-Host ""
-    Write-Host "━━━  $Title  " -ForegroundColor Cyan
+    Write-Host "===  $Title  " -ForegroundColor Cyan
 }
 
-function Write-Ok   { param ([string] $Msg) Write-Host "  ✔  $Msg" -ForegroundColor Green  }
-function Write-Skip { param ([string] $Msg) Write-Host "  –  $Msg" -ForegroundColor DarkGray }
-function Write-Fail { param ([string] $Msg) Write-Host "  ✖  $Msg" -ForegroundColor Red    }
-function Write-Running { param ([string] $Msg) Write-Host "  ⧖  $Msg" -ForegroundColor Yellow  }
+function Write-Ok {
+    param ([string] $Msg)
+    Write-Host "[OK]  $Msg" -ForegroundColor Green
+}
+
+function Write-Skip {
+    param ([string] $Msg)
+    Write-Host "[--]  $Msg" -ForegroundColor DarkGray
+}
+
+function Write-Fail {
+    param ([string] $Msg)
+    Write-Host "[X]  $Msg" -ForegroundColor Red
+}
+
+function Write-Running {
+    param ([string] $Msg)
+    Write-Host "[>>]  $Msg" -ForegroundColor Yellow
+}
