5134075c4c100d481815b889ff860a088bd53b20
joao-vcthr <joaovicthorm1@gmail.com>
2026-06-25 00:57:38 -0300

    Remove useless parameters

--- functions/Set/Set-KeyboardLayout.ps1
+++ functions/Set/Set-KeyboardLayout.ps1
@@ -1,5 +1,5 @@
 function Set-KeyboardLayout {
-    Write-Header "Removing unused keyboard layouts..." -ForegroundColor Cyan
+    Write-Header "Removing unused keyboard layouts..."
     $LanguageList = New-WinUserLanguageList "en-US"
     $LanguageList[0].InputMethodTips.Clear()
     $LanguageList[0].InputMethodTips.Add("0416:00010416")
