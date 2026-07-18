b9fb80416e09b93007a707276e86c9886544f577
joao-vcthr <joaovicthorm1@gmail.com>
2026-07-18 11:29:34 -0300

    Refactor: Simplified with 'Set-RegistryValue' helper

--- functions/Disable/Disable-MPO.ps1
+++ functions/Disable/Disable-MPO.ps1
@@ -1,12 +1,4 @@
 function Disable-MPO {
-    New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\Dwm" -Force | Out-Null
-
-    New-ItemProperty `
-        -Path "HKLM:\SOFTWARE\Microsoft\Windows\Dwm" `
-        -Name "OverlayTestMode" `
-        -PropertyType DWord `
-        -Value 5 `
-        -Force | Out-Null
-
+    Set-RegistryValue -Path "HKLM:\SOFTWARE\Microsoft\Windows\Dwm" -Name "OverlayTestMode" -Value 5
     Write-Ok "MPO disabled"
 }
